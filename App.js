8291ffa99b988d93276d86b547196be716bb0c46
izabelhillaryyy <izabelhillary@gmail.com>
2024-03-07 08:45:00 -0300

    pasta titulo

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Title from './title/';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>izabel hillary</Text>
-      <Text>Desenvolvimento de dispositivos moveis</Text>
+      <Text>aula dia 07-03-24</Text>
+      <Title/>
       <StatusBar style="auto" />
     </View>
   );
---
+++ title/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text} from "react-native";
+
+export default function title(){
+    return(
+      <View>
+        <text>Aula sobre clone e pastas</text>
+      </View>
+
+    );
+}
